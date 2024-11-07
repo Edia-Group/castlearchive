5d236013f629517163b061d5baafa230e4795ee5
filippo-mancinelli <filipp.mancinelli@studenti.unicam.it>
2024-11-07 12:58:41 +0100

    deploy 11

--- ecomm/medusa-config.js
+++ ecomm/medusa-config.js
@@ -4,7 +4,6 @@ try {
   dotenv.config();
 } catch (e) {}
 
-// Force development-like settings even in production
 const ADMIN_CORS = process.env.ADMIN_CORS || "http://localhost:7000,http://localhost:7001";
 const STORE_CORS = process.env.STORE_CORS || "http://localhost:8000";
 const DATABASE_URL = process.env.DATABASE_URL;
@@ -26,10 +25,27 @@ const plugins = [
         open: false,
       },
     },
+  },
+  // Adding back Stripe
+  {
+    resolve: `medusa-payment-stripe`,
+    options: {
+      api_key: process.env.STRIPE_API_KEY,
+      webhook_secret: process.env.STRIPE_WEBHOOK_SECRET,
+    },
+  },
+  // Adding back SendGrid
+  {
+    resolve: `medusa-plugin-sendgrid`,
+    options: {
+      api_key: process.env.SENDGRID_API_KEY,
+      from: "giannnlaa@gmail.com",
+      order_placed_template: "d-e69e46b356e7493c8dd7d0b692828f38",
+    },
   }
 ];
 
-// Force development-like module configuration
+// Development-like module configuration
 const modules = {
   eventBus: {
     resolve: "@medusajs/event-bus-local"
@@ -39,7 +55,6 @@ const modules = {
   }
 };
 
-// Development-like project configuration
 const projectConfig = {
   jwt_secret: process.env.JWT_SECRET,
   cookie_secret: process.env.COOKIE_SECRET,
