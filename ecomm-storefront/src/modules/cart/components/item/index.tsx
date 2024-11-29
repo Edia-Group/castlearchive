074dd1646e4ff5c40058faaab09b6bbc475e9c09
gitvanni <giovanni.sdringola51@gmail.com>
2024-11-29 10:28:00 +0100

    Added default background color to cart page and checkout page.

--- ecomm-storefront/src/modules/cart/components/item/index.tsx
+++ ecomm-storefront/src/modules/cart/components/item/index.tsx
@@ -46,7 +46,7 @@ const Item = ({ item, region, type = "full" }: ItemProps) => {
   }
 
   return (
-    <Table.Row className="w-full" data-testid="product-row">
+    <Table.Row className="w-full bg-def" data-testid="product-row">
       <Table.Cell className="!pl-0 p-4 w-24">
         <LocalizedClientLink
           href={`/products/${handle}`}
